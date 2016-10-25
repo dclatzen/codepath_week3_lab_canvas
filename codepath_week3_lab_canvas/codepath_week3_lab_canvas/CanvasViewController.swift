8c09da1a10859c034bd00acad3ba136cadd579fc
dclatzen <devon@studyblue.com>
2016-10-25 13:51:23 -0700

    Completed step 3.

---
+++ codepath_week3_lab_canvas/codepath_week3_lab_canvas/CanvasViewController.swift
@@ -0,0 +1,59 @@
+//
+//  CanvasViewController.swift
+//  codepath_week3_lab_canvas
+//
+//  Created by StudyBlue on 10/25/16.
+//  Copyright © 2016 myself. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        trayDownOffset = trayView.frame.height * 0.8
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x, y: trayView.center.y + trayDownOffset)
+        
+    // end viewDidLoad
+    }
+
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            var velocity = sender.velocity(in: view)
+            
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.trayView.center = self.trayDown
+                })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: { 
+                    self.trayView.center = self.trayUp
+                    print ("Tray moved up")
+                })
+            }
+            
+        } else {
+            print ("Something went wrong.")
+        }
+       
+    // end didPanTray
+    }
+
+
+}
